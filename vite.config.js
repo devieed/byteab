a32eda72ee92a5d7c043e87f0c642e86d02d9981
devieed <161264781+devieed@users.noreply.github.com>
2025-12-11 23:37:33 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/byteab/',
+  base: '/',
+  // base: '/byteab/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
